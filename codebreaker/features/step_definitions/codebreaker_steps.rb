f57fd9c922f5c07f63434f32f9330d39ac89b705
Simmi Badhan <simmi@addvalsolutions.com>
2015-02-05 13:54:52 +0530

    chapter 6

--- codebreaker/features/step_definitions/codebreaker_steps.rb
+++ codebreaker/features/step_definitions/codebreaker_steps.rb
@@ -11,14 +11,29 @@ def output1
   @output1 ||= Output1.new
 end
 
+# Codebreaker starts Game
 Given (/^I am not yet playing$/) do
 end
 
 When /^I start a new game$/ do 
   game = Codebreaker::Game.new(output1)
-  game.start
+  game.start('1234')
 end
 
 Then(/^I should see "(.*?)"$/) do |message|
   expect(output1.messages).to include(message)
+end
+
+# Codebreaker submits guess
+Given(/^the secret code is "(.*?)"$/) do |secret|
+  @game = Codebreaker::Game.new(output1)
+  @game.start(secret)
+end
+
+When(/^I guess "(.*?)"$/) do |guess|
+  @game.guess(guess)
+end
+
+Then(/^the mark should be "(.*?)"$/) do |mark|
+  expect(output1.messages).to include(mark)
 end
